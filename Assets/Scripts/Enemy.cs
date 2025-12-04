7f26641ea09cc39139de4d03dcc76df424427712
Arda Anıl Morkoç <ardamorkoc06@gmail.com>
2025-12-04 18:06:58 +0300

    ai düşmanlar eklendi, spawner mantığı eklendi

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform player;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
